fd213bba167a1e5a5446d91758992193cb638782
chieh <todaymofish@gmail.com>
2023-09-30 00:11:31 +0800

    remove: element UI

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstete from 'pinia-plugin-persistedstate'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
@@ -15,6 +13,5 @@ pinia.use(piniaPluginPersistedstete) // 持久化
 
 app.use(pinia)
 app.use(router)
-app.use(ElementPlus)
 
 app.mount('#app')
