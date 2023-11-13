60b8dc9fd2d5151b4b1344184ea9b014ce15039b
chieh <wangjie@healbone.xyz>
2023-11-13 15:25:01 +0800

    Set the alias of src to @

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // 路径别名
+    alias: {
+      extensions: "['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']",
+      '@': resolve(__dirname, 'src'),
+    }
+  },
   server: {
     open: true,
     port: 3005,
