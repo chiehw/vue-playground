f3e28143d567ba919d179d2f090baf090f600859
chieh <wangjie@healbone.xyz>
2023-11-13 13:50:00 +0800

    Init project, use tailwind

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+const getFont = () => {
+  const font = document.querySelector('.logo')
+  font.style.fontFamily = 'Roboto'
+}
+</script>
+
+<template>
+  <button @click="getFont" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Button
+  </button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import "./style/index.css"; //在此引入
+import App from './App.vue'
+
+
+createApp(App).mount('#app')
