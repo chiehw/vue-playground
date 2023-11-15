3d446684f155be60b9ef17528ef727c6b49ec3a2
chieh <wangjie@healbone.xyz>
2023-11-15 11:09:08 +0800

    Simplify MButton

--- src/components/MButton.vue
+++ src/components/MButton.vue
@@ -1,6 +1,7 @@
 <template>
-    <button class="m-button block cursor-pointer w-full bg-indigo-600
-         hover:bg-indigo-700 shadow rounded-md py-2 px-3 text-white text-sm font-semibold text-center
+    <button class="m-button bg-indigo-600 hover:bg-indigo-700 
+         rounded-lg py-2 px-3 
+         text-white text-sm font-bold 
          ">
         <slot />
     </button>
